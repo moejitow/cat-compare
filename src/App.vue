7fbfee120bde89f336f4d1fc6e0a4e56b5efe6e5
moejitow <juliuskiefer@web.de>
2020-10-22 00:59:35 +0200

    :lipstick: Added navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header :title=title :pages=pages :image=image />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './layouts/Header' 
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
+  },
+  data() {
+    return {
+    title: "Cat Compare",
+    pages: ["How To", "About"],
+    image: "cat.svg" 
+    }
   }
 }
 </script>
@@ -23,6 +29,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/layouts/Header.vue
@@ -0,0 +1,65 @@
+<template>
+    <header>
+        <div id="logo">
+            <img v-if="image" alt="Vue logo" :src="require(`@/assets/${image}`)">
+             <h1>{{ title }}</h1>
+        </div>
+        <div id="nav">
+            <h2 class="link" v-for="page in pages" :key=page>{{ page }}</h2>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    props: {
+        image: {
+            required: false,
+            type: String
+        },
+        title: {
+            required: true,
+            type: String
+        },
+        pages: {
+            required: true,
+            type: Array
+        }
+    },
+}
+</script>
+
+<style scoped>
+header {
+    background: #669DB3FF;
+    color: #DFDCE5FF;
+
+    max-height: 6vw;
+    height: auto;
+
+    display: flex;
+    align-items: center;
+
+    justify-content: space-between;
+}
+#logo {
+    display: flex;
+    align-items: center;
+}
+#nav {
+    display: flex;
+    align-items: center;
+}
+img {
+    max-height: 6vw;
+    height: auto;
+}
+h1 {
+    font-size: 3vw;
+    padding-left: 1vw;
+}
+.link {
+    padding-right: 2vw;
+}
+</style>
