dc093228d6c2494f843381459f341be4ad027e1e
Julius Kiefer <juliuskiefer@web.de>
2020-10-24 14:24:14 +0200

    :lipstick: Added time slider

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <Timer v-if="!timerStarted" @game-started="startGame"/>
   </div>
 </template>
 
 <script>
+import Timer from '@/components/Timer'
 export default {
   name: 'Home',
+  components: {
+    Timer
+  },
+  data: function() {
+    return {
+      time: 10,
+      timerStarted: false
+    }
+  },
+  methods: {
+    startGame (value) {
+      this.time = value
+      this.timerStarted = true
+    }
+  }
 }
 </script>
+
+<style scoped>
+.home {
+  padding: 5em;
+}
+</style>
