0c1b3c1b4d0be2e44ada1204353d7004cb01db36
Julius Kiefer <juliuskiefer@web.de>
2020-10-26 00:02:13 +0100

    :lipstick: Added sum of votes

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -2,6 +2,12 @@
   <v-container>
     <v-row class="justify-center"><h1>Summary</h1></v-row>
     <v-row class="justify-space-between">
+      <v-alert color="grey darken-1" dark icon="mdi-sigma">
+        Total
+      </v-alert>
+      {{ allVotes }}
+    </v-row>
+    <v-row class="justify-space-between">
       <v-alert type="success">
         Likes
       </v-alert>
@@ -29,6 +35,13 @@ export default {
     votes: {
       type: Object,
       required: true,
+    },
+  },
+  computed: {
+    allVotes() {
+      return (
+        this.votes.likes + this.votes.dislikes + this.votes.skipped
+      );
     },
   },
 };
