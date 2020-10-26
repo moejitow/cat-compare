236323d5a4172498fd54fdfadb57a42567306740
Julius Kiefer <juliuskiefer@web.de>
2020-10-26 17:40:31 +0100

    :lipstick: Added about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <h1>About</h1>
+        This Website was created to showcase the features of the
+        Vue.js framework.
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h1>Icons</h1>
+        <div>
+          Icons made by
+          <a
+            href="https://www.flaticon.com/authors/freepik"
+            title="Freepik"
+          >
+            Freepik
+          </a>
+          from
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
